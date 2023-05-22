d2925a30e02b0a3e238a47929474e999f7698f3d
Alicia Sykes <alicia@omg.lol>
2023-05-23 00:52:29 +0100

    Refactored and resolved issues in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,61 @@
-
 mod fetch;
 mod ui;
 mod widgets;
 mod welcome;
 
-use fetch::fetch_query_log::fetch_adguard_query_log;
-use fetch::fetch_stats::fetch_adguard_stats;
-use fetch::fetch_status::fetch_adguard_status;
-
-use ui::{ draw_ui };
-use std::{sync::Arc, time::Duration, env};
-use reqwest::{Client};
+use fetch::{
+    fetch_query_log::fetch_adguard_query_log, 
+    fetch_stats::fetch_adguard_stats, 
+    fetch_status::fetch_adguard_status
+};
+use ui::draw_ui;
+use reqwest::Client;
+use std::{env, sync::Arc, time::Duration};
 use tokio::time::interval;
 
-async fn run() -> Result<(), anyhow::Error> {
-
+async fn run() -> anyhow::Result<()> {
     let shutdown = Arc::new(tokio::sync::Notify::new());
 
-    // Channels for sending data from the fetcher to the UI
     let (queries_tx, queries_rx) = tokio::sync::mpsc::channel(1);
     let (stats_tx, stats_rx) = tokio::sync::mpsc::channel(1);
     let (status_tx, status_rx) = tokio::sync::mpsc::channel(1);
 
-    let draw_ui_task = tokio::spawn(draw_ui(queries_rx, stats_rx, status_rx, Arc::clone(&shutdown)));
+    let draw_ui_task = tokio::spawn(
+        draw_ui(queries_rx, stats_rx, status_rx, Arc::clone(&shutdown))
+    );
 
     let client = Client::new();
 
-    let ip = env::var("ADGUARD_IP").unwrap();
-    let port = env::var("ADGUARD_PORT").unwrap();
-    let hostname = &(format!("http://{}:{}", ip, port));
-    let username = &env::var("ADGUARD_USERNAME").unwrap();
-    let password = &env::var("ADGUARD_PASSWORD").unwrap();
-    
-
+    let ip = env::var("ADGUARD_IP")?;
+    let port = env::var("ADGUARD_PORT")?;
+    let hostname = format!("http://{}:{}", ip, port);
+    let username = env::var("ADGUARD_USERNAME")?;
+    let password = env::var("ADGUARD_PASSWORD")?;
 
     let interval_secs: u64 = env::var("ADGUARD_UPDATE_INTERVAL")
-        .unwrap_or("3".to_string())
-        .parse()
-        .expect("Failed to parse ADGUARD_UPDATE_INTERVAL into a u64");
+        .unwrap_or_else(|_| "3".into()).parse()?;
 
     let mut interval = interval(Duration::from_secs(interval_secs));
     
     loop {
         tokio::select! {
             _ = interval.tick() => {
-                let queries = fetch_adguard_query_log(&client, hostname, username, password).await?;
-                if queries_tx.try_send(queries.data).is_err() {
-                    break;
+                let queries = fetch_adguard_query_log(
+                    &client, &hostname, &username, &password).await?;
+                if queries_tx.send(queries.data).await.is_err() {
+                    return Err(anyhow::anyhow!("Failed to send query data"));
                 }
                 
-                let stats = fetch_adguard_stats(&client, hostname, username, password).await?;
-                if stats_tx.try_send(stats).is_err() {
-                    break;
+                let stats = fetch_adguard_stats(
+                    &client, &hostname, &username, &password).await?;
+                if stats_tx.send(stats).await.is_err() {
+                    return Err(anyhow::anyhow!("Failed to send stats data"));
                 }
 
-                let status = fetch_adguard_status(&client, hostname, username, password).await?;
-                if status_tx.try_send(status).is_err() {
-                    break;
+                let status = fetch_adguard_status(
+                    &client, &hostname, &username, &password).await?;
+                if status_tx.send(status).await.is_err() {
+                    return Err(anyhow::anyhow!("Failed to send status data"));
                 }
             }
             _ = shutdown.notified() => {
@@ -64,33 +63,24 @@ async fn run() -> Result<(), anyhow::Error> {
             }
         }
     }
-    
+
     draw_ui_task.await??;
+
     Ok(())
 }
 
 fn main() {
     let rt = tokio::runtime::Runtime::new().unwrap();
     rt.block_on(async {
-        if let Err(e) = welcome::welcome().await {
+        welcome::welcome().await.or_else(|e| {
             eprintln!("Failed to initialize: {}", e);
-            std::process::exit(1);
-        }
+            Err(std::io::Error::new(std::io::ErrorKind::Other, "Failed to initialize"))
+        }).unwrap();
 
-        if let Err(e) = run().await {
+        run().await.or_else(|e| {
             eprintln!("Failed to run: {}", e);
-            std::process::exit(1);
-        }
+            Err(std::io::Error::new(std::io::ErrorKind::Other, "Failed to run"))
+        }).unwrap();
     });
 }
-
-
-
-// fn main() -> Result<(), anyhow::Error> {
-//     let rt = tokio::runtime::Builder::new_current_thread()
-//         .enable_all()
-//         .build()?;
-
-//     rt.block_on(run())
-// }
 
