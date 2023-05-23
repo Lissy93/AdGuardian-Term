64201be5529c00df06f0edf98a362d5c058b85bc
Alicia Sykes <alicia@omg.lol>
2023-05-23 21:56:16 +0100

    Refactored welcome.rs

--- src/welcome.rs
+++ src/welcome.rs
@@ -1,21 +1,16 @@
 use std::io::{self, Write};
-// use std::io::{Error, ErrorKind};
 use std::env;
 use reqwest::{Client, Error};
 use colored::*;
 
-
-fn print_error(address: String, error: Option<Error>) {
-    println!("{}", format!("Failed to connect to AdGuard at {}", address).red());
-    match error {
-        Some(err) => { 
-            println!("{}", format!("{}", err).red().dimmed());
-        },
-        None => { println!("Didn't get anything"); }
-    }
-    println!("{}", "\nPlease check your environmental variables and try again.".yellow());
-    println!("{}", "Exiting...".blue());
+fn print_info(text: &str, is_secondary: bool) {
+    let text = if is_secondary {
+        println!("{}", text.green().italic().dimmed());
+    } else {
+        println!("{}", text.green());
+    };
 }
+
 
 fn print_ascii_art() {
     let art = r"
@@ -26,47 +21,47 @@ fn print_ascii_art() {
 ██║  ██║██████╔╝╚██████╔╝╚██████╔╝██║  ██║██║  ██║██████╔╝██║██║  ██║██║ ╚████║
 ╚═╝  ╚═╝╚═════╝  ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
 ";
-    println!("{}", art.green());
+    print_info(art, false);
+    print_info("\nWelcome to AdGuardian Terminal Edition!", false);
+    print_info("Terminal-based, real-time traffic monitoring and statistics for your AdGuard Home instance", true);
+    print_info("For documentation and support, please visit: https://github.com/lissy93/adguardian-term\n", true);
+}
 
-    println!("{}", "\nWelcome to AdGuardian Terminal Edition!".green());
-    println!("{}", "Terminal-based, real-time traffic monitoring and statistics for your AdGuard Home instance".green().italic().dimmed());
-    println!("{}", "For documentation and support, please visit: https://github.com/lissy93/adguardian-term\n".green().italic().dimmed());
+fn print_error(address: &str, error: Option<&Error>) {
+    eprintln!(
+        "{}{}",
+        format!("Failed to connect to AdGuard at {}", address).red(),
+        match error {
+            Some(err) => format!("\n{}", err).red().dimmed(),
+            None => ColoredString::from("".red().dimmed()),
+        },
+    );
+    eprintln!("{}\n{}", "\nPlease check your environmental variables and try again.".yellow(), "Exiting...".blue());
 }
 
 
-pub async fn welcome() -> Result<(), Box<dyn std::error::Error>> {
-    print_ascii_art();
+fn get_env(key: &str) -> Result<String, env::VarError> {
+    env::var(key).map(|v| {
+        println!(
+            "{}",
+            format!(
+                "{} is set to {}",
+                key.bold(),
+                if key.contains("PASSWORD") { "******" } else { &v }
+            )
+            .green()
+        );
+        v
+    })
+}
 
-    println!("{}", "Starting initialization checks...".blue());
-
-    let client = Client::new();
-
-    for &key in &["ADGUARD_IP", "ADGUARD_PORT", "ADGUARD_USERNAME", "ADGUARD_PASSWORD"] {
-        if env::var(key).is_err() {
-            println!("{}", format!("The {} environmental variable is not yet set", key.bold()).yellow());
-    
-            print!("{}", format!("› Enter a value for {}: ", key).blue().bold());
-            io::stdout().flush()?;
-    
-            let mut value = String::new();
-            io::stdin().read_line(&mut value)?;
-            env::set_var(key, value.trim());
-        }
-    
-        if key.contains("PASSWORD") {
-            println!("{}", format!("{} is set to ******", key.bold()).green());
-        } else {
-            println!("{}", format!("{} is set to {}", key.bold(), env::var(key).unwrap()).green());
-        }
-    }
-    
-
-    let ip = env::var("ADGUARD_IP").unwrap();
-    let port = env::var("ADGUARD_PORT").unwrap();
-    let username = env::var("ADGUARD_USERNAME").unwrap();
-    let password = env::var("ADGUARD_PASSWORD").unwrap();
-
-
+async fn verify_connection(
+    client: &Client,
+    ip: String,
+    port: String,
+    username: String,
+    password: String,
+) -> Result<(), Box<dyn std::error::Error>> {
     println!("{}", "\nVerifying connection to your AdGuard instance...".blue());
 
     let auth_string = format!("{}:{}", username, password);
@@ -76,22 +71,43 @@ pub async fn welcome() -> Result<(), Box<dyn std::error::Error>> {
 
     let url = format!("http://{}:{}/control/status", ip, port);
 
-    let res = match client.get(&url).headers(headers).send().await {
-        Ok(response) => response,
-        Err(e) => {
-            print_error(format!("{}:{}", ip, port), Some(e));
+    match client.get(&url).headers(headers).send().await {
+        Ok(res) if res.status().is_success() => {
+            println!("{}", "AdGuard connection successful!\n".green());
+            Ok(())
+        }
+        Ok(_) | Err(_) => {
+            print_error(&format!("{}:{}", ip, port), None);
             std::process::exit(1);
-        },
-    };
+        }
+    }
+}
 
-    if res.status().is_success() {
-        println!("{}", "AdGuard connection successful!\n".green());
-    } else {
-        print_error(format!("{}:{}", ip, port), None);
-        std::process::exit(1);
+pub async fn welcome() -> Result<(), Box<dyn std::error::Error>> {
+    print_ascii_art();
+    println!("{}", "Starting initialization checks...".blue());
+
+    let client = Client::new();
+
+    for &key in &["ADGUARD_IP", "ADGUARD_PORT", "ADGUARD_USERNAME", "ADGUARD_PASSWORD"] {
+        if env::var(key).is_err() {
+            println!(
+                "{}",
+                format!("The {} environmental variable is not yet set", key.bold()).yellow()
+            );
+            print!("{}", format!("› Enter a value for {}: ", key).blue().bold());
+            io::stdout().flush()?;
+
+            let mut value = String::new();
+            io::stdin().read_line(&mut value)?;
+            env::set_var(key, value.trim());
+        }
     }
 
+    let ip = get_env("ADGUARD_IP")?;
+    let port = get_env("ADGUARD_PORT")?;
+    let username = get_env("ADGUARD_USERNAME")?;
+    let password = get_env("ADGUARD_PASSWORD")?;
 
-
-    Ok(())
+    verify_connection(&client, ip, port, username, password).await
 }
