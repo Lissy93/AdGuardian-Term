41a934b23e5ec55e23de79bf65ee2357f3da148f
Lissy93 <Lissy93@users.noreply.github.com>
2025-12-27 22:03:59 +0000

    deploy: b0e46c2a8a52f650612ee2e50e9f798b7ae7cb81

--- src-files.js
+++ src-files.js
@@ -1,3 +1,2 @@
-var srcIndex = new Map(JSON.parse('[["adguardian",["",[["fetch",[],["fetch_filters.rs","fetch_query_log.rs","fetch_stats.rs","fetch_status.rs","mod.rs"]],["widgets",[],["chart.rs","filters.rs","gauge.rs","list.rs","mod.rs","status.rs","table.rs"]]],["main.rs","ui.rs","welcome.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[247]}
+createSrcSidebar('[["adguardian",["",[["fetch",[],["fetch_filters.rs","fetch_query_log.rs","fetch_stats.rs","fetch_status.rs","mod.rs"]],["widgets",[],["chart.rs","filters.rs","gauge.rs","list.rs","mod.rs","status.rs","table.rs"]]],["main.rs","ui.rs","welcome.rs"]]]]');
+//{"start":19,"fragment_lengths":[247]}
